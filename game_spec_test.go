985aab61bd28f97d626d54c79bf2701161c7fe69
Jamie Ly <me@jamie.ly>
2013-09-28 23:19:59 -0400

    Check position specs passing

--- game_spec_test.go
+++ game_spec_test.go
@@ -7,6 +7,7 @@ import (
 func GameSpec(c Context) {
   origin := NewIndex(0, 0)
   east := NewDirection(1, 0)
+  north := NewDirection(0, 1)
 
   c.Specify("It should have directions.", func() {
     c.Expect(len(NewGame().directions), Equals, 8)
@@ -31,10 +32,37 @@ func GameSpec(c Context) {
     c.Expect(game.CheckPosition(origin, A, east, 1), IsTrue)
   })
   c.Specify("should return true when checkPosition is called with an index and the marker at that index, the same marker beneath it, and 2 steps", func() {
-    c.Expect(true, IsFalse)
+    game := NewGame()
+    c.Expect(game.DropIntoColumn(A, 0), Not(IsNil))
+    c.Expect(game.DropIntoColumn(A, 0), Not(IsNil))
+    c.Expect(game.CheckPosition(origin, A, north, 2), IsTrue)
   })
-  c.Specify("can check whether there are markers in a row", func() {
-    c.Expect(true, IsFalse)
+  c.Specify("can check whether there are markers in a row 1", func() {
+    game := NewGame()
+    game.DropIntoColumn(A, 0)
+    game.DropIntoColumn(A, 1)
+    game.DropIntoColumn(A, 2)
+    c.Expect(game.CheckPosition(origin, A, east, 4), IsFalse)
+    game.DropIntoColumn(A, 3)
+    c.Expect(game.CheckPosition(origin, A, east, 4), IsTrue)
+  })
+  c.Specify("can check whether there are markers in a row 2", func() {
+    game := NewGame()
+    game.DropIntoColumn(A, 0)
+    game.DropIntoColumn(A, 1)
+    game.DropIntoColumn(A, 2)
+    c.Expect(game.CheckPosition(origin, A, east, 4), IsFalse)
+    game.DropIntoColumn(B, 3)
+    index_1_0 := NewIndex(1,0)
+    c.Expect(game.CheckPosition(index_1_0, A, east, 4), IsFalse)
+    game.DropIntoColumn(A, 0)
+    c.Expect(game.CheckPosition(index_1_0, A, east, 4), IsFalse)
+    game.DropIntoColumn(A, 1)
+    c.Expect(game.CheckPosition(index_1_0, A, east, 4), IsFalse)
+    game.DropIntoColumn(A, 2)
+    c.Expect(game.CheckPosition(index_1_0, A, east, 4), IsFalse)
+    game.DropIntoColumn(A, 3)
+    c.Expect(game.CheckPosition(index_1_0, A, east, 4), IsTrue)
   })
   c.Specify("should be a player's win whenever there are four in a row", func() {
     c.Expect(true, IsFalse)
